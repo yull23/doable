7c2cdca5881fa7d04f687022f4038510b2bf35db
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-09-02 02:54:44 -0500

    Adding functions for obtaining requests, for the user, session and task services

--- index.js
+++ index.js
@@ -6,14 +6,56 @@ import { HomePage } from "./scripts/pages/home.js";
 import STORE from "./scripts/store.js";
 import { Tasks } from "./scripts/components/tasks.js";
 import { createUser, deleteUser } from "./scripts/services/user-service.js";
+import {
+  createTask,
+  deleteTask,
+  editTask,
+  listTasks,
+  showTask,
+} from "./scripts/services/task-service.js";
+
+// DATA USER
 const credentials = { email: "yull.timoteo.z@uni.pe", password: "yull23" };
 const newCredentials = { email: "yull_6.timoteo.z@uni.pe", password: "yull23" };
+// DATA TASK
+let idTask = 931;
+const newTask = {
+  title: "New task",
+  due_date: "2021-09-30",
+};
+
+const contentEdit = {
+  title: "Yull_Edit",
+  due_date: "2021-10-01",
+  important: true,
+  completed: true,
+};
+
+let lastId;
 
 async function init() {
   try {
     const token = sessionStorage.getItem(tokenKey);
-    const newUser = await createUser(newCredentials);
-    await deleteUser();
+    console.log(token);
+    const user = await loginUser(credentials);
+    console.log(user);
+    console.log(token);
+    // const newUser = await createUser(newCredentials);
+    // await deleteUser();
+
+    // START:  Comprobación de funcionamiento de task-service
+    const idSearch = await showTask(idTask);
+    console.log(idSearch);
+    const taskCreated = await createTask(newTask);
+    console.log(taskCreated);
+    const taskEdit = await editTask(idTask, contentEdit);
+    console.log(taskEdit);
+    const tasksList = await listTasks();
+    console.log(tasksList);
+    lastId = tasksList.slice(-1)[0].id;
+    await deleteTask(lastId);
+    console.log(await listTasks());
+    // END:  Comprobación de funcionamiento de task-service
 
     // if (!token) return DOMHandler.load(FormLogin);
     // console.log("Is at Home");
--- scripts/services/task-service.js
+++ scripts/services/task-service.js
@@ -1,7 +1,85 @@
-import { tokenKey } from "../config.js";
 import apiFetch from "./api-fetch.js";
 
-//
 export function listTasks() {
   return apiFetch("/tasks");
+  // Retorno :
+  // [
+  // 	{
+  // 		"id": 931,
+  // 		"title": "Edited",
+  // 		"due_date": "2021-10-01",
+  // 		"important": true,
+  // 		"completed": true,
+  // 		"user_id": 216,
+  // 		"created_at": "2023-08-31T01:45:11.760Z",
+  // 		"updated_at": "2023-09-02T07:36:57.942Z"
+  // 	},...
+  // ]
+}
+
+export function showTask(id) {
+  return apiFetch(`tasks/${id}`);
+  // Entrada de showTask(contentEdit) segun API es el ID
+  // Retorno :
+  // {
+  // 	"id": 931,
+  // 	"title": "Edited",
+  // 	"due_date": "2021-10-01",
+  // 	"important": true,
+  // 	"completed": true,
+  // 	"user_id": 216,
+  // 	"created_at": "2023-08-31T01:45:11.760Z",
+  // 	"updated_at": "2023-09-02T07:36:57.942Z"
+  // }
+}
+
+export function createTask(task) {
+  return apiFetch("tasks", { body: task });
+  // Entrada de createTask(task) segun API
+  // const newTask = {
+  //   title: "New task",
+  //   due_date: "2021-09-30",
+  // };
+  /// Retorno de createTask()=>
+
+  // {
+  // 	"id": 1080,
+  // 	"title": "New task",
+  // 	"due_date": "2021-09-30",
+  // 	"important": false,
+  // 	"completed": false,
+  // 	"user_id": 216,
+  // 	"created_at": "2023-09-02T07:33:49.243Z",
+  // 	"updated_at": "2023-09-02T07:33:49.243Z"
+  // }
+}
+
+export function editTask(id, contentEdit) {
+  return apiFetch(`tasks/${id}`, { method: "PATCH", body: contentEdit });
+  // Entrada de editTask(contentEdit) segun API
+  // {
+  // 	"title": "Edited",
+  // 	"due_date": "2021-10-01",
+  // 	"important": true,
+  // 	"completed": true
+  // }
+
+  /// Retorno de editTask()=>
+
+  // {
+  // 	"user_id": 216,
+  // 	"title": "Edited",
+  // 	"due_date": "2021-10-01",
+  // 	"completed": true,
+  // 	"important": true,
+  // 	"id": 931,
+  // 	"created_at": "2023-08-31T01:45:11.760Z",
+  // 	"updated_at": "2023-09-02T07:36:57.942Z"
+  // }
+}
+
+export function deleteTask(id) {
+  apiFetch(`tasks/${id}`, { method: "DELETE" });
+  // Entrada de deleteTask(contentEdit) segun API es el ID
+  /// la función retorna el estado 204.
 }
